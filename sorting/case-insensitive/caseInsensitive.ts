b71a95987778afc198372fa8ccb28ef15878795d
Eric Huie <erichuie97@gmail.com>
2024-03-01 12:04:17 -0800

    added caseInsensitive.ts

--- sorting/case-insensitive/caseInsensitive.ts
+++ sorting/case-insensitive/caseInsensitive.ts
@@ -1,7 +1,18 @@
 /** Sort array of strings in-place, case-insensitively. Returns array. */
 
 function caseInsensitiveSort(arr: string[]): string[] {
-  return [];
+  arr.sort(function(a:string, b:string): number{
+    const lowerA = a.toLowerCase();
+    const lowerB = b.toLowerCase();
+    if(lowerA < lowerB){
+      return -1;
+    }
+    if(lowerA > lowerB){
+      return 1;
+    }
+    return 0;
+  })
+  return arr;
 }
 
 export { caseInsensitiveSort };
