0ab2a577ce563f3cac96d483571b5ed2858b546b
Nathan Rydel <nathanrydel@gmail.com>
2024-03-01 13:13:28 -0700

    updated bubbleSort

--- sorting/bubble-sort/bubbleSort.ts
+++ sorting/bubble-sort/bubbleSort.ts
@@ -1,13 +1,6 @@
 /** Sort in place and returns sorted array. */
 
 function bubbleSort<T>(nums: T[]): T[] {
-  for (let i = nums.length; i > 0; i--) {
-    for (let j = 0; j < i - 1; j++) {
-      if (nums[j] > nums[j + 1]) {
-        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
-      }
-    }
-  }
 
   return nums;
 }
